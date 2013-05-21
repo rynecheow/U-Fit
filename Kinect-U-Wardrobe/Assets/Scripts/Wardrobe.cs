e29d5f44eac0d52eb24fbb06d2647d419bbea093
Kok Hong Choo <kokhong200@gmail.com>
2013-05-21 14:37:11 +0800

    commt

--- Kinect-U-Wardrobe/Assets/Scripts/Wardrobe.cs
+++ Kinect-U-Wardrobe/Assets/Scripts/Wardrobe.cs
@@ -4,8 +4,12 @@ using System.Collections;
 public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+	    Mesh mesh = gameObject.GetComponent<SkinnedMeshRenderer>().sharedMesh;
+		Vector3 [] vertices = mesh.vertices;
+		vertices[0].y = vertices[0].y + 3;
+		gameObject.GetComponent<SkinnedMeshRenderer>().sharedMesh.vertices = vertices;
 	}
 	
 	// Update is called once per frame
