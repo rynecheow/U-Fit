497ea732923afc581c9ba43edd331856cab4544b
Kok Hong Choo <kokhong200@gmail.com>
2013-05-27 17:51:39 +0800

    Consider done?

--- Kinect-U-Wardrobe/Assets/Scripts/HorizontalProgressBar.cs
+++ Kinect-U-Wardrobe/Assets/Scripts/HorizontalProgressBar.cs
@@ -10,44 +10,39 @@ public class HorizontalProgressBar : MonoBehaviour {
  
 	void Start() {
 		
-//		Texture2D texture = new Texture2D(150, 20);
-//        emptyTex = texture;
-//		fullTex = texture;
-//		
-//        int y = 0;
-//        while (y < texture.height) {
-//            int x = 0;
-//            while (x < texture.width) {
-//                Color color = Color.blue;
-//                texture.SetPixel(x, y, color);
-//                ++x;
-//            }
-//            ++y;
-//        }
-//        texture.Apply();
-
 		pos = new Vector2(Screen.width/2,Screen.height*4/5);
 		size  = new Vector2(150,20);
+		
+//		emptyTex = new Texture2D(300,20);
+//		fullTex = new Texture2D(300,20);
+		
+		emptyTex =(Texture2D) Resources.Load("white");
+		fullTex =(Texture2D)  Resources.Load("blue");
+
 		Debug.Log(pos);
 	}
     void OnGUI() {
-       //draw the background:
-       GUI.BeginGroup(new Rect(pos.x, pos.y, size.x, size.y));
-         GUI.Box(new Rect(0,0, size.x, size.y), emptyTex);
- 
-         //draw the filled-in part:
-         GUI.BeginGroup(new Rect(0,0, size.x * barDisplay, size.y));
-          GUI.Box(new Rect(0,0, size.x, size.y), fullTex);
-         GUI.EndGroup();
-       GUI.EndGroup();
+		GUI.BeginGroup(new Rect(pos.x,pos.y, size.x, size.y));
+        GUI.DrawTexture(new Rect(0,0, size.x, size.y), emptyTex);
+        GUI.EndGroup();
+		
+		GUI.BeginGroup(new Rect(pos.x,pos.y, size.x * barDisplay, size.y));
+        GUI.DrawTexture(new Rect(0,0, size.x, size.y), fullTex);
+        GUI.EndGroup();
     }
  
     void Update() {
-       //for this example, the bar display is linked to the current time,
-       //however you would set this value based on your desired display
-       //eg, the loading progress, the player's health, or whatever.
-       barDisplay = Time.time*0.05f;
-//   barDisplay = MyControlScript.staticHealth;
+       	//for this example, the bar display is linked to the current time,
+       	//however you would set this value based on your desired display
+       	//eg, the loading progress, the player's health, or whatever.
+		
+       	barDisplay = Time.time*0.05f;
+	   	if(size.x * barDisplay >150)
+		{
+			Time.time = 0;
+		}
+		
+	//   barDisplay = MyControlScript.staticHealth;
     }
 
 }
