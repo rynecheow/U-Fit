bc775ebad4503f927ba6b2c877dcb0b1e7a0cb06
Ryne Cheow <rynec92@hotmail.com>
2013-05-29 00:22:45 +0800

    code ref

--- Kinect-U-Wardrobe/Assets/Scripts/HorizontalProgressBar.cs
+++ Kinect-U-Wardrobe/Assets/Scripts/HorizontalProgressBar.cs
@@ -2,56 +2,51 @@ using UnityEngine;
 using System.Collections;
  
 public class HorizontalProgressBar : MonoBehaviour {
-    public float barDisplay; //current progress
-    public Vector2 pos;
-    public Vector2 size;
-    public Texture2D emptyTex;
-    public Texture2D fullTex;
-	public float startTime;
-	public GUIText subText;
-	public GUIText mainText;
-	public static float progressBarSize;
+	
+    public float         barDisplay; //current progress
+	public float         startTime;
+	public static float  progressBarSize;
+    public Vector2       position;
+    public Vector2       size;
+    public Texture2D     emptyTex;
+    public Texture2D     fullTex;
+	public GUIText       subText;
+	public GUIText       mainText;
+	
  
 	void Start() {
 		
-		pos = new Vector2(Screen.width/3 + Screen.width/10,Screen.height*4/5);
-		size  = new Vector2(150,20);
+		position   = new Vector2 ( (13 * Screen.width)/30, (4 * Screen.height)/5 );
+		size       = new Vector2 ( 150, 20 );
 		
-		emptyTex =(Texture2D) Resources.Load("white");
-		fullTex =(Texture2D)  Resources.Load("lightgray");
+		emptyTex   = (Texture2D) Resources.Load("white");
+		fullTex    = (Texture2D)  Resources.Load("lightgray");
 		
 		startTime = 0;
 		progressBarSize = 0;
 		
 	}
     void OnGUI() {
-		
-		if(SkeletonController.userId!=0)
-		{
-			GUI.BeginGroup(new Rect(pos.x,pos.y, size.x, size.y));
+		if(SkeletonController.userId!=0){
+			GUI.BeginGroup(new Rect(position.x,position.y, size.x, size.y));
 	        GUI.DrawTexture(new Rect(0,0, size.x, size.y), emptyTex);
 	        GUI.EndGroup();
 			
-			GUI.BeginGroup(new Rect(pos.x,pos.y, size.x * barDisplay, size.y));
+			GUI.BeginGroup(new Rect(position.x,position.y, size.x * barDisplay, size.y));
 	        GUI.DrawTexture(new Rect(0,0, size.x, size.y), fullTex);
 	        GUI.EndGroup();
-		
 		}
     }
  
     void Update() {
-		if(SkeletonController.userId!=0)
-		{
+		if(SkeletonController.userId!=0){
 			startTime = Time.time - SkeletonController.detectTime;
        		barDisplay = startTime*0.2f;
 			subText.text = "";
 			mainText.text = "Loading...";
-			mainText.transform.position = new Vector3(subText.transform.position.x, mainText.transform
-				.position.y);
+			mainText.transform.position = new Vector3(subText.transform.position.x, mainText.transform.position.y);
 			
 			progressBarSize = size.x * barDisplay;
 		}
-		
 	}
-	
 }
--- Kinect-U-Wardrobe/Assets/Scripts/SkeletonController.cs
+++ Kinect-U-Wardrobe/Assets/Scripts/SkeletonController.cs
@@ -4,102 +4,90 @@ using OpenNI;
 
 public class SkeletonController : MonoBehaviour 
 {
-	public OpenNIUserTracker UserTracker;
-	public OpenNISkeleton[] Skeletons;
-	public static OpenNISkeleton[] staticSkeleton;
-	
-	public static int userId;
-	public static float detectTime;
-	private bool firstRun = true;
-	private bool outOfFrame;
+   public static     OpenNISkeleton[]     staticSkeleton;
+   public static     int                  userId;
+   public static     float                detectTime;
+
+	public   OpenNIUserTracker  UserTracker;
+	public   OpenNISkeleton[]   Skeletons;
+
+	private  bool  firstRun   = true;
+	private  bool  outOfFrame       ;
 	
 	public bool IsTracking {
-		get { return userId != 0; }
+		get {
+         return userId != 0;
+      }
 	}
 	
 	// Use this for initialization
 	
-	void Start()
-	{
-        if (!UserTracker) {
-            UserTracker = GetComponent<OpenNIUserTracker>();
-		}
-		if (!UserTracker) {
-			UserTracker = GameObject.FindObjectOfType(typeof(OpenNIUserTracker)) as OpenNIUserTracker;
-        }
-        if (!UserTracker) {
-            Debug.LogWarning("Missing a User Tracker. Adding...");
-            UserTracker = gameObject.AddComponent<OpenNIUserTracker>();
-        }
+   void Start(){
+      if (!UserTracker) {
+         UserTracker = GetComponent<OpenNIUserTracker>();
+      }
+      if (!UserTracker) {
+         UserTracker = GameObject.FindObjectOfType(typeof(OpenNIUserTracker)) as OpenNIUserTracker;
+      }
+      if (!UserTracker) {
+         Debug.LogWarning("Missing a User Tracker. Adding...");
+         UserTracker = gameObject.AddComponent<OpenNIUserTracker>();
+      }
 		
-		if (UserTracker.MaxCalibratedUsers < 1) {
-			UserTracker.MaxCalibratedUsers = 1;
-		}
-	}
+      if (UserTracker.MaxCalibratedUsers < 1) {
+         UserTracker.MaxCalibratedUsers = 1;
+      }
+   }
 	
 	// Update is called once per frame
-	void Update () 
-	{
+	void Update (){
 		// do we have a valid calibrated user?
-		if (0 != userId)
-		{
+		if (0 != userId){
 			// is the user still valid?
-			if (!UserTracker.CalibratedUsers.Contains(userId))
-			{
+			if (!UserTracker.CalibratedUsers.Contains(userId)){
 				userId = 0;
-				foreach (OpenNISkeleton skel in Skeletons)
-				{
+				foreach (OpenNISkeleton skel in Skeletons){
 					skel.RotateToCalibrationPose();
 				}
 			}
 		}
 		
 		// look for a new userId if we dont have one
-		if (0 == userId)
-		{
+		if (0 == userId){
 			// just take the first calibrated user
-			if (UserTracker.CalibratedUsers.Count > 0)
-			{				
+			if (UserTracker.CalibratedUsers.Count > 0){
 				userId = UserTracker.CalibratedUsers[0];
 				outOfFrame = false;
 			}
 		}
 		
 		// we have a valid userId, lets use it for something!
-		if (0 != userId)
-		{
+		if (0 != userId){
 			// see if user is out o'frame
 			Vector3 com = UserTracker.GetUserCenterOfMass(userId);
-			if (outOfFrame != (com == Vector3.zero))
-			{
+			if (outOfFrame != (com == Vector3.zero)){
 				outOfFrame = (com == Vector3.zero);
 				SendMessage("UserOutOfFrame", outOfFrame, SendMessageOptions.DontRequireReceiver);
 			}
 			
 			// update our skeleton based on active user id	
-			foreach (OpenNISkeleton skel in Skeletons)
-			{
+			foreach (OpenNISkeleton skel in Skeletons){
 				UserTracker.UpdateSkeleton(userId, skel);
 			}
 			
 			staticSkeleton = Skeletons;
 			
-			if(firstRun)
-			{
+			if(firstRun){
 				detectTime = Time.time;
 				firstRun = false;
 			}
 		}
 	}
 	
-	void OnGUI()
-	{
-		if (userId == 0)
-		{
+	void OnGUI(){
+		if (userId == 0){
 
-		}
-		else
-		{
+		}else{
 			// Calibrated
 			GUILayout.BeginVertical("box");
 			GUILayout.Label(string.Format("Calibrated: {0}", userId));
