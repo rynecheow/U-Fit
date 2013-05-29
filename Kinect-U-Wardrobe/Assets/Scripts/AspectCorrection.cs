72886da9fac5c860cf4a7c50f607a61cf8e41d73
Ryne Cheow <rynec92@hotmail.com>
2013-05-29 10:55:44 +0800

    =)

--- Kinect-U-Wardrobe/Assets/Scripts/AspectCorrection.cs
+++ Kinect-U-Wardrobe/Assets/Scripts/AspectCorrection.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class AspectCorrection : MonoBehaviour{
  
    void Start () {
-      float currentRatio = (float)Screen.width / Screen.height;
 
       guiText.fontSize = Screen.width > 1400 ? 90 : 40;
       guiText.pixelOffset = new Vector2(Screen.width/9.0f, 0.0f);
