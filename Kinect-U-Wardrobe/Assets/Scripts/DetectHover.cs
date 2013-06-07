32dc11278ac3f147e576653ed820113335a001ed
Kok Hong <kokhong200@gmail.com>
2013-06-07 22:50:24 +0800

    commit for backup

--- Kinect-U-Wardrobe/Assets/Scripts/DetectHover.cs
+++ Kinect-U-Wardrobe/Assets/Scripts/DetectHover.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class DetectHover : MonoBehaviour {
 	public GameObject shirt;
+	Vector3 center;
+	Bounds bounds;
 	// Use this for initialization
 	void Start () {
-	
+		// First find a center for your bounds.
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Debug.Log("XXX" + transform.position.x + "YYY" + transform.position.y);
 	}
 }
