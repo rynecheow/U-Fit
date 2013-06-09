3cb2dd6a890a8bdc08cd425d8c7a762aa11ba65c
Kok Hong <kokhong200@gmail.com>
2013-06-09 15:32:48 +0800

    Can detect from cursor

--- Kinect-U-Wardrobe/Assets/Scripts/DetectHover.cs
+++ Kinect-U-Wardrobe/Assets/Scripts/DetectHover.cs
@@ -2,26 +2,48 @@ using UnityEngine;
 using System.Collections;
 
 public class DetectHover : MonoBehaviour {
-	public Collider collider;
-	Vector3 center;
+	public GameObject[] hahas;
+	public string hahaName;
 	// Use this for initialization
 	void Start () {
 		// First find a center for your bounds.
+		hahaName = "NULL";
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
 	
-	void OnTriggerEnter(Collider col)
-	{
-	    if (col.gameObject.name == "CollidorTry")
-	    {
+//	void OnTriggerEnter(Collider col)
+//	{
+//	    if (col.gameObject.name == "CollidorTry")
+//	    {
+//			
+//	    }
+// 
+//}
+    void Update() {
+		for (int i = 0 ; i <hahas.Length ; i++)
+		{
+			if (guiTexture.HitTest(Camera.main.WorldToScreenPoint(hahas[i].transform.position)))
+			{
+				hahaName = hahas[i].name;
+				break;
+			}
 			
-	    }
- 
-}
+			hahaName = "NULL";
+		}
+    
+    }
+	
+	void OnGUI()
+	{
+		GUILayout.BeginArea (new Rect (Screen.width/2, Screen.height/2, 500, 500));
+		if(!hahaName.Equals("NULL"))
+		{
+			GUILayout.Box(hahaName + " is Detected");
+		}
+		GUILayout.EndArea();
+	}
+
 
 }
